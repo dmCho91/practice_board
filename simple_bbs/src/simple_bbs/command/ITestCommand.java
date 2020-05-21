33b5042dcdac90e63be26e1c1622eaba96f74b5c
JaneDoe <JaneDoe@JaneDoe-PC>
2020-05-21 16:12:30 +0900

    remove Unnecessary things

--- simple_bbs/src/simple_bbs/command/ITestCommand.java
+++
@@ -1,9 +0,0 @@
-package simple_bbs.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ITestCommand {
-
-	public int execute(HttpServletRequest request, HttpServletResponse response);
-}
